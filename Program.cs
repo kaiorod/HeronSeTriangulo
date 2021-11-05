84c182405af0a9237f325198dce7eedab0c37645
kaiorod <caiorodrigo2775@gmail.com>
2021-11-05 20:34:46 -0300

    Finaliza HeronSeTriangulo

--- Program.cs
+++ Program.cs
@@ -8,19 +8,39 @@ namespace HeronSeTriangulo
         {
             double a, b, c, A, s;
 
-            Console.WriteLine("Digite os lados do triangulo");
+            Console.WriteLine("Digite os lados do triangulo\n");
+
             Console.Write("Lado a : ");
             a = double.Parse(Console.ReadLine());
 
-            Console.Write("Lado b : ")
+            Console.Write("Lado b : ");
             b = double.Parse(Console.ReadLine());
 
-            Console.Write("Lado c : ")
+            Console.Write("Lado c : ");
             c = double.Parse(Console.ReadLine());
 
-            if (a < (b + c) && a > Math.Abs(b - c) && b < (a + c) && b > Math.Abs(a - c) && c < (a + b) && c > Math.Abs(a - b))
+            s = (a + b + c) / 2;
+            A = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+            if(a < (b + c) && a > Math.Abs(b - c) && b < (a + c ) && b > Math.Abs(a - c) && c < (a + b) && c > Math.Abs(a - b))
             {
-                if (a)
+                if(a == b && b == c)
+                {
+                    Console.WriteLine("\n-- Triângulo Equilátero --");
+                    Console.WriteLine($"Área = {A:N2}");
+                    
+                }
+                else if(a != b && a != c && b != c)
+                {
+                    Console.WriteLine("\n-- Triângulo Escaleno --");
+                    Console.WriteLine($"Área = {A:N2}");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("\n-- Triângulo Isóceles --");
+                    Console.WriteLine($"Área = {A:N2}");
+                }
             }
         }
     }
